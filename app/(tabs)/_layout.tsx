c4210746ad02ea620e9a1ab35e6cb050f6049165
kasdeya <kas@nnnetwork.xyz>
2025-04-07 22:24:11 -0700

    added navbar and home page is default page

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,71 @@
+import { Image, StyleSheet } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { icons } from "@/constants/icons";
+
+const TabIcon = ({ focused, icon }: { focused: boolean; icon: any }) => {
+  return <Image source={focused ? icon.focused : icon.unfocused} />;
+};
+
+const TabLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "000000",
+        tabBarInactiveTintColor: "#4F7396",
+        tabBarShowLabel: true,
+        tabBarItemStyle: {},
+        tabBarStyle: {
+          backgroundColor: "#F7FAFC",
+          borderColor: "#E8EDF2",
+          borderTopWidth: 1,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="wishlist"
+        options={{
+          title: "Wishlist",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.heart} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="orders"
+        options={{
+          title: "Orders",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.orders} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.profile} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
+
+const styles = StyleSheet.create({});
